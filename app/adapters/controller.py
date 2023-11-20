aa906b1dfea04f6a4412666ffd0ada21e44c9516
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-11-20 15:39:44 +0100

    Update controller

--- app/adapters/controller.py
+++ app/adapters/controller.py
@@ -1,13 +1,15 @@
+from ..application.use_cases.username_password_auth \
+    import UsernamePasswordAuthInteractor
 
 
-class Controller:
+class UsernamePasswordController:
     """_summary_
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, use_case: UsernamePasswordAuthInteractor) -> None:
+        self._use_case = use_case
 
-    @staticmethod
-    def username_password_auth(username: str, password: str) -> None:
-        ...
-
+    def username_password_auth(self, username: str, password: str) -> None:
+        username_standardized = username.lower()
+        self._use_case.do(username=username_standardized,
+                          input_password=password)
