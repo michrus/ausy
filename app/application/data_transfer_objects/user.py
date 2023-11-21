ae39c1db31d8fb68181b98299dc36ea4c21dd24c
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-11-21 09:19:21 +0100

    Rename dataclasses dir to data_transfer_objects

--- app/application/data_transfer_objects/user.py
+++ app/application/data_transfer_objects/user.py
@@ -4,6 +4,12 @@ from dataclasses import dataclass
 @dataclass
 class UserData:
     id: str
-    password_hash: str
     name: str
     email: str
+    access_level: str
+
+
+@dataclass
+class UserHash:
+    user_id: str
+    password_hash: str
