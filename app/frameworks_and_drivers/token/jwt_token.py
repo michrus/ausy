0ebaa11c8cd2a51e4727397184eb1b83f6801ad1
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-11-21 10:46:40 +0100

    Implement JWT token generator

--- app/frameworks_and_drivers/token/jwt_token.py
+++ app/frameworks_and_drivers/token/jwt_token.py
@@ -1,8 +1,28 @@
+from datetime import datetime, timedelta
+from typing import Any
+
+import jwt
+
 from ...application.interfaces.token_generator import TokenGenerator
-from ...application.dataclasses.token import TokenData
+from ...application.data_transfer_objects.token import TokenData
 
 
 class JWTToken(TokenGenerator):
 
-    def generate(self) -> TokenData:
-        ...
+    def __init__(self,
+                 secret_key: str,
+                 expiration_period: timedelta,
+                 algorithm: str = "HS256") -> None:
+        super().__init__(secret_key=secret_key,
+                         expiration_period=expiration_period)
+        self._algorithm = algorithm
+
+    def generate(self, payload: dict[str, Any]) -> TokenData:
+        expiry_datetime = datetime.utcnow() + self._expiration_period
+        expiry_timestamp = int(expiry_datetime.timestamp())
+        p = payload | {"exp": expiry_timestamp}
+        token: str = jwt.encode(p, self._secret_key, self._algorithm)
+        token_data = TokenData(token_value=token,
+                               expiration_timestamp=expiry_timestamp,
+                               token_type="Bearer")
+        return token_data
