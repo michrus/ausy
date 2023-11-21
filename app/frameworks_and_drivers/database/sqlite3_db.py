f6e94b3a0e950297ee7c03bb34dba6262aeee461
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-11-21 09:19:59 +0100

    Update sqlite3 db access implementation

--- app/frameworks_and_drivers/database/sqlite3_db.py
+++ app/frameworks_and_drivers/database/sqlite3_db.py
@@ -1,7 +1,10 @@
-from ...application.interfaces.database import DatabaseAccess, UserDTO
+from ...application.interfaces.database import DatabaseAccess
+from ...application.data_transfer_objects.user import UserData, UserHash
 
 
 class SQLite3Database(DatabaseAccess):
+    def get_user_by_username(self, username: str) -> UserData:
+        ...
 
-    def get_user_by_username(self, username: str) -> UserDTO:
+    def get_hash_by_username(self, username: str) -> UserHash:
         ...
