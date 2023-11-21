4f195103ad41121e2115774ef4e79ea6556eff1d
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-11-21 09:19:42 +0100

    Split user data and user hash retrieving

--- app/application/interfaces/database.py
+++ app/application/interfaces/database.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
 
-from ..dataclasses.user import UserData
+from ..data_transfer_objects.user import UserData, UserHash
 
 
 class DatabaseAccess(ABC):
 
     @abstractmethod
     def get_user_by_username(self, username: str) -> UserData:
+        ...
+
+    @abstractmethod
+    def get_hash_by_username(self, username: str) -> UserHash:
         ...
