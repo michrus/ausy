417123e71b1c4d7cef4c6930a379f5c89c4326de
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-11-21 09:57:35 +0100

    AccessLevel enum and serialization method

--- app/domain/user.py
+++ app/domain/user.py
@@ -1,3 +1,10 @@
+from enum import Enum, auto
+from typing import Union
+
+
+class AccessLevel(Enum):
+    ADMIN = auto()
+
 
 class User:
 
@@ -10,13 +17,22 @@ class User:
         self._user_id: str = user_id
         self._username: str = username
         self._email: str = email
-        self._access_level: str = access_level
+        self._access_level: AccessLevel = AccessLevel[access_level.upper()]
         self._password_hash: str = password_hash
 
     def change_password(self, current_password: str, new_password: str) -> bool:
         self.validate_password(current_password)
         new_password_hash = self._hash_password(new_password)
         self._password_hash = new_password_hash
+
+    def serialize(self) -> dict[str, str]:
+        return {
+            "user_id": self._user_id,
+            "username": self._username,
+            "email": self._email,
+            "access_level": self._access_level.name,
+            "password_hash": self._password_hash
+        }
 
     def validate_password(self, input_password: str) -> None:
         input_password_hash = self._hash_password(input_password)
