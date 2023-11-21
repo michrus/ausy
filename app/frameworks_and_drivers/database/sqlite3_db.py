7cacbdb0a955f95ecf44a353fee62e4c56bdc865
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-11-21 12:14:31 +0100

    SQLite3 implementation of DatabaseAccess

--- app/frameworks_and_drivers/database/sqlite3_db.py
+++ app/frameworks_and_drivers/database/sqlite3_db.py
@@ -1,10 +1,24 @@
-from ...application.interfaces.database import DatabaseAccess
-from ...application.data_transfer_objects.user import UserData, UserHash
+import os
+import sqlite3
+from typing import Any
+
+from ...adapters.database_access import DatabaseAccess
 
 
-class SQLite3Database(DatabaseAccess):
-    def get_user_by_username(self, username: str) -> UserData:
-        ...
+class LowLevelEagerSQLite3Database(DatabaseAccess):
+    def __init__(self, db_name: str, db_dir_path: str) -> None:
+        super().__init__(db_name=db_name)
+        self._db_path: str = os.path.join(db_dir_path, db_name)
 
-    def get_hash_by_username(self, username: str) -> UserHash:
-        ...
+    def get_by_column(self,
+                      table_name: str,
+                      column_name: str,
+                      column_value: str) -> list[dict[str, Any]]:
+        with sqlite3.connect(self._db_path) as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            query = f'''SELECT * FROM {table_name} WHERE {column_name} = ?'''
+            cursor.execute(query, (column_value,))
+            rows = cursor.fetchall()
+        rows_as_dicts = [dict(r) for r in rows]
+        return rows_as_dicts
