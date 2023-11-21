7b13ca0f7a0e95106c2c9c493265399699566364
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-11-21 10:46:15 +0100

    Updated TokenGenerator abstract class

--- app/application/interfaces/token_generator.py
+++ app/application/interfaces/token_generator.py
@@ -1,9 +1,16 @@
 from abc import ABC, abstractmethod
+from datetime import timedelta
+from typing import Any
 
-from ..dataclasses.token import TokenData
+from ..data_transfer_objects.token import TokenData
 
 
 class TokenGenerator(ABC):
+
+    def __init__(self, secret_key: str, expiration_period: timedelta) -> None:
+        self._secret_key = secret_key
+        self._expiration_period = expiration_period
+
     @abstractmethod
-    def generate(self) -> TokenData:
+    def generate(self, payload: dict[str, Any]) -> TokenData:
         ...
