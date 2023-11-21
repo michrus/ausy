a6bc606911fc67afcf92ab03622c9018b06261d0
Michał Chruściński <m.chruscinski1994@gmail.com>
2023-11-21 09:42:00 +0100

    Update User class to contain more information and methods

--- app/application/use_cases/username_password_auth.py
+++ app/application/use_cases/username_password_auth.py
@@ -1,11 +1,11 @@
-from ..data_transfer_objects.user import UserData
+from ..data_transfer_objects.user import UserData, UserHash
 from ..data_transfer_objects.username_password_auth_response_model \
     import UsernamePasswordAuthResponseModel
 from ..interfaces.database import DatabaseAccess
 from ..interfaces.token_generator import TokenGenerator
 from ..interfaces.username_password_auth_response \
     import UsernamePasswordAuthOutputBoundary
-from ...domain.user import User
+from ...domain.user import User, UserException
 
 
 
@@ -22,22 +22,23 @@ class UsernamePasswordAuthInteractor:
 
     def do(self, username: str, input_password: str):
         user_data: UserData = self._db_access.get_user_by_username(username)
-        user = User(username=user_data.name,
-                    password_hash=user_data.password_hash)
+        user_hash:UserHash = self._db_access.get_hash_by_username(username)
+        user = User(user_id=user_data.id,
+                    username=user_data.name,
+                    email=user_data.email,
+                    access_level=user_data.access_level,
+                    password_hash=user_hash.password_hash)
         response = UsernamePasswordAuthResponseModel()
 
-        if user.validate_password(input_password=input_password):
-            try:
-                token = self._token_generator.generate()
-                response.message = "Authentication successful"
-                response.token = token
-                response.user = user_data
-                response.success = True
-            except Exception as e:
-                response.success = False
-                response.message = str(e.with_traceback())
-        else:
+        try:
+            user.validate_password(input_password=input_password)
+            token = self._token_generator.generate()
+            response.message = "Authentication successful"
+            response.token = token
+            response.user = user_data
+            response.success = True
+        except UserException as user_exception:
             response.success = False
-            response.message = "Incorrect username or password"
+            response.message = str(user_exception)
 
         self._presenter.form_response(response)
--- app/domain/user.py
+++ app/domain/user.py
@@ -1,13 +1,35 @@
 
 class User:
 
-    def __init__(self, username: str, password_hash: str) -> None:
-        self._username = username
-        self._password_hash = password_hash
+    def __init__(self,
+                 user_id: str,
+                 username: str,
+                 email: str,
+                 access_level: str,
+                 password_hash: str) -> None:
+        self._user_id: str = user_id
+        self._username: str = username
+        self._email: str = email
+        self._access_level: str = access_level
+        self._password_hash: str = password_hash
 
-    def validate_password(self, input_password: str):
+    def change_password(self, current_password: str, new_password: str) -> bool:
+        self.validate_password(current_password)
+        new_password_hash = self._hash_password(new_password)
+        self._password_hash = new_password_hash
+
+    def validate_password(self, input_password: str) -> None:
         input_password_hash = self._hash_password(input_password)
-        return input_password_hash == self._password_hash
+        if input_password_hash != self._password_hash:
+            raise UserWrongPasswordError()
 
     def _hash_password(self, password: str) -> str:
         ...
+
+class UserException(Exception):
+    """Generic exception for User class."""
+    pass
+
+class UserWrongPasswordError(UserException):
+    """Raised when password validation fails."""
+    pass
