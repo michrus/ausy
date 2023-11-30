11e47e7ac685e879f7519c771ebd44d5149ac8b9
Michal Chruscinski <m.chruscinski1994@gmail.com>
2023-11-30 13:28:35 +0100

    Add varchar size in DDLs

--- data/fixtures/user_hash.sql
+++ data/fixtures/user_hash.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS user_hash (
-    user_id VARCHAR PRIMARY KEY,
-    password_hash VARCHAR NOT NULL
+    user_id VARCHAR(255) PRIMARY KEY,
+    password_hash VARCHAR(65535) NOT NULL
 );
--- data/fixtures/user_hash_algorithm.sql
+++ data/fixtures/user_hash_algorithm.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS user_hash_algorithm (
-    user_id VARCHAR PRIMARY KEY,
-    hash_algorithm VARCHAR NOT NULL
+    user_id VARCHAR(255) PRIMARY KEY,
+    hash_algorithm VARCHAR(255) NOT NULL
+    algorithm_parameters VARCHAR(65535) NOT NULL
 );
