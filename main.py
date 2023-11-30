8eec99e0010f4f607bc8de8f3681e83d5bc1edb0
Michal Chruscinski <m.chruscinski1994@gmail.com>
2023-11-30 11:43:41 +0100

    Fixes in main module

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import secrets
 from datetime import timedelta
 
-from app.frameworks_and_drivers.server.flask_router import app, g
+from src.frameworks_and_drivers.server.flask_router import app, g
 
-from app.adapters.api_presenter import ApiAuthTokenPresenter
-from app.adapters.username_password_controller import UsernamePasswordController
-from app.application.use_cases.username_password_auth \
+from src.adapters.api_presenter import ApiAuthTokenPresenter
+from src.adapters.database.multi_database import MultiDatabaseAccess
+from src.adapters.username_password_controller import UsernamePasswordController
+from src.application.use_cases.username_password_auth \
     import UsernamePasswordAuthInteractor
-from app.frameworks_and_drivers.database.sqlite3_db \
+from src.frameworks_and_drivers.database.sqlite3_db \
     import LowLevelEagerSQLite3Database
-from app.frameworks_and_drivers.token.jwt_token import JWTTokenGenerator
+from src.frameworks_and_drivers.token.jwt_token import JWTTokenGenerator
 
 
 if __name__ == "__main__":
 
-    low_level_eager_sqlite3_db = LowLevelEagerSQLite3Database(db_name="test.db",
-                                                              db_dir_path="./")
+    user_db_access = LowLevelEagerSQLite3Database(db_name="user.db",
+                                                  db_dir_path="./")
+    hash_db_access = LowLevelEagerSQLite3Database(db_name="hash.db",
+                                                  db_dir_path="./")
+    multi_database_access = MultiDatabaseAccess(user_data_access=user_db_access,
+                                                user_data_table="user_data",
+                                                user_hash_access=hash_db_access,
+                                                user_hash_table="user_hash")
 
     secrets_hex = secrets.token_hex(32)
     one_hour = timedelta(hours=1)
@@ -24,7 +31,7 @@ if __name__ == "__main__":
 
     presenter = ApiAuthTokenPresenter()
     username_password_interactor = UsernamePasswordAuthInteractor(
-        data_access=low_level_eager_sqlite3_db,
+        data_access=multi_database_access,
         token_generator=jwt_generator,
         presenter=presenter
     )
